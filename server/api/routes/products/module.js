97511b79d1a85e9ff73aa5960801157ff9fc5f44
smaudd <smauddproject@gmail.com>
2019-04-21 16:14:27 +0200

    Module Update

---
+++ server/api/routes/products/module.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const create_product = require('./products_CRUD/create');
+const read_product = require('./products_CRUD/read');
+const update_product = require('./products_CRUD/update');
+const delete_product = require('./products_CRUD/delete');
+const create_category = require('./categories_CRUD/create');
+const read_category = require('./categories_CRUD/read');
+const update_category = require('./categories_CRUD/update');
+const delete_category = require('./categories_CRUD/delete');
+
+router.use('/create', create_product);
+router.use('/read', read_product);
+router.use('/update', update_product);
+router.use('/delete', delete_product);
+router.use('/categories/create/', create_category);
+router.use('/categories/read/', read_category);
+router.use('/categories/update/', update_category);
+router.use('/categories/delete/', delete_category);
+
+module.exports = router;
